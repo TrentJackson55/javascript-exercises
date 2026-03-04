a2c055ef27532e9c1da4c172fcb7923c5444fab3
TrentJackson55 <TrentJackson55@users.noreply.github.com>
2026-03-04 15:55:51 -0800

    Submit solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...elements) {
+    /*let chosenElementIndex = arr.indexOf(element, 0);
+    arr.splice(chosenElementIndex, 1);
+    return arr;
+    */
+    let elementIndex = 0;
 
+    for (const el of elements) {
+        while (arr.includes(el)) {
+            elementIndex = arr.indexOf(el, 0);
+            arr.splice(elementIndex, 1);
+        }
+        
+    }
+    return arr;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
