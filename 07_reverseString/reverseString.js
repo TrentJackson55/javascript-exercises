be0831f015018783ed714f98c040bb13ddd4a727
TrentJackson55 <TrentJackson55@users.noreply.github.com>
2026-03-04 15:57:06 -0800

    Submit solutions

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = str.split('');
+    arr.reverse();
 
+    let reversedString = arr.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
